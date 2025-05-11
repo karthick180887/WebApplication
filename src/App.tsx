ec3304f0ef8d58b536905a2fe0bb1a4f7ff54af6
karthick180887 <karthickspikey@gmail.com>
2025-05-11 11:26:37 +0530

    Set up authentication and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <h1>Web Console Remote Access Application</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
