32973cf929cba73b980426dc6e1d3025cebfbb4b
karthick180887 <karthickspikey@gmail.com>
2025-05-11 11:24:20 +0530

    Create main.tsx entry point

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1>Web Console Remote Access Application</h1>
+    </div>
+  )
+}
+
+export default App
